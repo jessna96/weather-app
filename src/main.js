20e9458f8ae8f8a96ae8091dfe558d96096e723b
Jessica <35634254+jessna96@users.noreply.github.com>
2023-02-02 08:54:21 +0100

    Set up vuetify and deleted unnecessary files

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from "vue";
 import { createPinia, PiniaVuePlugin } from "pinia";
 
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/router";
 
 import "./assets/main.css";
 
+import vuetify from "./plugins/vuetify";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.use(PiniaVuePlugin);
@@ -13,5 +14,6 @@ Vue.use(PiniaVuePlugin);
 new Vue({
   router,
   pinia: createPinia(),
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
